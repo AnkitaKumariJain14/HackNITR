189e9ec92b06afdc176cd7b27a75b49a3b5ecd59
Sajid Hussain Rafi Ahamed <50620562+sajiddeboss@users.noreply.github.com>
2021-03-21 09:09:38 +0530

    Delete core directory

--- core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .views import *
-
-admin.site.register(About)
-admin.site.register(Service)
-admin.site.register(RecentWork)
-admin.site.register(Client)
--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import HomeTemplateView
-from .views import DiseaseView
-from . import views
-urlpatterns = [
-    path('', HomeTemplateView.as_view()),
-    path('disease',DiseaseView.as_view()),
-    path('result',views.result,name='result'),
-]
